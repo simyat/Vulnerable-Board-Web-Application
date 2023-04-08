caa009502da2aa6a8f1b816b2113b66ee5b276f3
inmoG <inmoy@naver.com>
2023-04-09 01:26:36 +0900

    modified oracle.sql

--- JSP-with-vuln/src/main/java/db/oracle.sql
+++ JSP-with-vuln/src/main/java/db/oracle.sql
@@ -1,39 +1,32 @@
 -- users
 CREATE TABLE users (
-  id number not null,
-  user_id VARCHAR2(20) NOT NULL,
+  user_id VARCHAR2(15) NOT NULL,
   name VARCHAR2(20) NOT NULL,
   password VARCHAR2(20) not null,
-  address VARCHAR2(30) not null,
+  address VARCHAR2(50) not null,
   regidate date default sysdate not null,
-  constraint users_user_id unique(user_id),
-  PRIMARY KEY(user_id, name)
+  primary key(user_id)
 );
 
--- sequence
-create sequence seq_users_id
-  increment by 1
-  start with 1
-  minvalue 1
-  nomaxvalue
-  nocycle
-  nocache;
+-- drop sequence
+drop sequence seq_users_id
 
 -- board
 create table board (
- id number not null primary key,
-user_id varchar2(20) not null,
-name varchar(20) not null,
-title varchar(200) not null,
-content CLOB not null,
-postdate date default sysdate not null,
-original_file varchar2(200),
-save_file varchar2(40),
-down_count number(6) default 0 not null,
-visit_count number(6) default 0 not null,
-like_count number(6) default 0 not null,
-CONSTRAINT fk_board_users FOREIGN KEY (user_id, name) REFERENCES users(user_id, name)
+  id number not null primary key,
+  user_id varchar2(15) not null,
+  name varchar(20) not null,
+  title varchar(200) not null,
+  content CLOB not null,
+  postdate date default sysdate not null,
+  original_file varchar2(200),
+  save_file varchar2(40),
+  visit_count number(6) default 0 not null,
+  like_count number(6) default 0 not null
 );
+
+-- drop sequence
+drop sequence seq_board_id
 
 -- sequence
 create sequence seq_board_id
@@ -43,3 +36,8 @@ create sequence seq_board_id
   nomaxvalue
   nocycle
   nocache;
+
+-- foreign key
+alter table board
+  add constraint board_users_fk foreign key (user_id)
+  references users(user_id);
