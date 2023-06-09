35e25107cdecd69787a12d01e3dfb2ce94c8df48
inmoG <inmoy@naver.com>
2023-06-09 13:01:12 +0900

    file upload

--- JSP-with-vuln/src/main/java/Controller/CommunityWriteController.java
+++ JSP-with-vuln/src/main/java/Controller/CommunityWriteController.java
@@ -2,37 +2,57 @@ package Controller;
 
 import java.io.IOException;
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.servlet.MultipartConfigElement;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
 import model.CommunityDAO;
 import model.CommunityDTO;
 
 @WebServlet("/community/write")
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2, maxFileSize = 1024 * 1024 * 10, maxRequestSize = 1024 * 1024 * 30)
 public class CommunityWriteController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         req.setCharacterEncoding("UTF-8");
-        String title = req.getParameter("title");
-        String content = req.getParameter("content");
-        
         HttpSession session = req.getSession();
         String userId = (String) session.getAttribute("UserId");
         String userName = (String) session.getAttribute("UserName");
+
+        Collection<Part> parts = req.getParts();
+        Iterator<Part> part = parts.iterator();
+        String title = null;
+        String content = null;
+        String fileName = null;
+
+        while (part.hasNext()) {
+            title = req.getParameter(part.next().getName());
+            content = req.getParameter(part.next().getName());
+            Part filePart = req.getPart("file");
+            fileName = fileUpload(filePart, part.next());
+        }
 
         CommunityDTO dto = new CommunityDTO();
         dto.setUser_id(userId);
         dto.setName(userName);
         dto.setTitle(title);
         dto.setContent(content);
+        dto.setOriginal_file(fileName);
         CommunityDAO dao = new CommunityDAO();
         int id = dao.CommunityWrite(dto);
         if (id > 0) {
-            resp.sendRedirect("/hackthebox/community/posts?id="+id);
+            resp.sendRedirect("/hackthebox/community/posts?id=" + id);
         } else {
             try {
                 // 권한없는 사용자가 작성 시 예외 발생
@@ -55,5 +75,13 @@ public class CommunityWriteController extends HttpServlet {
             RequestDispatcher dispatcher = req.getRequestDispatcher("../api/login.jsp");
             dispatcher.forward(req, resp);
         }
+    }
+
+    private String fileUpload(Part filePart, Part part) throws IOException {
+        String fileName = filePart.getSubmittedFileName();
+
+        // part.write("F:\\uploads\\" + fileName);
+        part.write("/var/lib/tomcat9/webapps/uploads" + fileName);
+        return fileName;
     }
 }
--- JSP-with-vuln/src/main/webapp/community/write.jsp
+++ JSP-with-vuln/src/main/webapp/community/write.jsp
@@ -1,30 +1,28 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%> 
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <script src="<%= request.getContextPath() %>/js/validate.js" charset="utf-8"></script>
 <script src="<%= request.getContextPath() %>/js/file.js" charset="utf-8"></script>
 
-<%
-    if (session.getAttribute("UserId") == null) { 
-%> 
+<% if (session.getAttribute("UserId") == null) { %>
 <script>
     validateAccess();
 </script>
 <% } else { %>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>글쓰기</title>
-</head>
-<body>
-    <form action="/hackthebox/community/write" method="post" enctype="multipart/form-data">
-        <input type="text" name="title" id="" required>
-        <textarea name="content" id="" required></textarea>
-        <input type="file" name="file" id="attachment"><button type="button" onclick="uploadFiles()">파일 업로드</button>
-        <input type="submit" value="submit">
-    </form>
-    <button onclick="history.back()">취소</button>
-</body>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>글쓰기</title>
+    </head>
+    <body>
+        <form action="/hackthebox/community/write" method="post" enctype="multipart/form-data">
+            <input type="text" name="title" id="" required />
+            <textarea name="content" id="" required></textarea>
+            <input type="file" name="file" id="attachment" />
+            <input type="submit" value="submit" />
+        </form>
+        <button onclick="history.back()">취소</button>
+    </body>
 </html>
 <% } %>
